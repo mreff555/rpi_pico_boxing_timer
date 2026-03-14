b99a6dfb8c67dffa8204072259b85acd8d3035bd
Dan Feerst <feerstd@gmail.com>
2026-03-13 18:28:58 -0600

    Fixed time interface in button class (untested)

--- src/button.cpp
+++ src/button.cpp
@@ -3,97 +3,103 @@
 #include "button.pio.h"
 
 Button::Button(PIO pio, uint8_t gpio, std::string buttonDesc)
-: pio(pio), gpioId(gpio), buttonState(DEFAULT_STATE), timeStamp(time(NULL)), sm(0), offset(0)
+    : pio(pio), gpioId(gpio), buttonState(DEFAULT_STATE), sm(0), offset(0)
 {
-  if(buttonDesc == "")
-  {
-    this->mButtonDesc = std::string("gpio ") + std::to_string(gpio);
-  }
-  else
-  {
-    this->mButtonDesc = buttonDesc;
-  }
+    rtc_get_datetime(&timeStamp);
 
-  // Claim an unused state machine
-  sm = pio_claim_unused_sm(this->pio, true);
-  
-  // Add the program
-  offset = pio_add_program(this->pio, &button_program);
-  
-  // Initialize the program
-  button_program_init(this->pio, sm, offset, gpio);
-  printf("Using gpio %d\n", gpio);  // DEBUG
- }
+    if(buttonDesc == "")
+    {
+        this->mButtonDesc = std::string("gpio ") + std::to_string(gpio);
+    }
+    else
+    {
+        this->mButtonDesc = buttonDesc;
+    }
+
+    // Claim an unused state machine
+    sm = pio_claim_unused_sm(this->pio, true);
+    
+    // Add the program
+    offset = pio_add_program(this->pio, &button_program);
+    
+    // Initialize the program
+    button_program_init(this->pio, sm, offset, gpio);
+    printf("Using gpio %d\n", gpio);  // DEBUG
+}
 
 void Button::update()
 {
-  switch(buttonState)
-  {
-    case DEFAULT_STATE:
-      if(pio_sm_get(pio, sm) == 1)
-      {
-        buttonState = RISING_EDGE;
-      }
-      break;
-    case RISING_EDGE:
-      if(pio_sm_get(pio, sm) == 1)
-      {
-        if(timeElapsedExceeds(timeStamp, BUTTON_LONG_HOLD_SECONDS))
-        {
-          buttonState = LONG_HOLD;
-        }
-      }
-      else if(pio_sm_get(pio, sm) == 0)
-      {
-        buttonState = FALLING_EDGE;
-      }
-      break;
-    case LONG_HOLD:
-      if(pio_sm_get(pio, sm) == 0)
-      {
-        buttonState = FALLING_EDGE;
-      }
-      break;
-    case FALLING_EDGE:
-        if(pio_sm_get(pio, sm) == 0)
-        {
-            if(timeElapsedExceeds(timeStamp, BUTTON_DEFAULT_STATE_TRANSITION_SECONDS))
+    switch(buttonState)
+    {
+        case DEFAULT_STATE:
+            if(pio_sm_get(pio, sm) == 1)
             {
-                buttonState = DEFAULT_STATE;
+                buttonState = RISING_EDGE;
             }
-        }
-        else if(pio_sm_get(pio, sm) == 1)
-        {
-            buttonState = RISING_EDGE;
-        }
-      break;
-  } 
-
-  // TODO: This is compiling but I am not sure if it will work correctly.
-  timeStamp = time(NULL);
+            break;
+        case RISING_EDGE:
+            if(pio_sm_get(pio, sm) == 1)
+            {
+                if(timeElapsedExceeds(timeStamp, BUTTON_LONG_HOLD_SECONDS))
+                {
+                    buttonState = LONG_HOLD;
+                }
+            }
+            else if(pio_sm_get(pio, sm) == 0)
+            {
+                buttonState = FALLING_EDGE;
+            }
+            break;
+        case LONG_HOLD:
+            if(pio_sm_get(pio, sm) == 0)
+            {
+                buttonState = FALLING_EDGE;
+            }
+            break;
+        case FALLING_EDGE:
+            if(pio_sm_get(pio, sm) == 0)
+            {
+                if(timeElapsedExceeds(timeStamp, BUTTON_DEFAULT_STATE_TRANSITION_SECONDS))
+                {
+                    buttonState = DEFAULT_STATE;
+                }
+            }
+            else if(pio_sm_get(pio, sm) == 1)
+            {
+                buttonState = RISING_EDGE;
+            }
+            break;
+    } 
+    rtc_get_datetime(&timeStamp);
 }
 
 uint8_t Button::getGpio() const { return gpioId; }
 
 ButtonState Button::getState() const { return buttonState; }
 
-time_t Button::getTimeStamp() const { return timeStamp; }
+datetime_t Button::getTimeStamp() const { return timeStamp; }
 
 std::string Button::getButtonDesc() const { return mButtonDesc; }
 
 bool Button::operator<<(const Button &rhs)
 {
-  bool success = false;
-  if (this->gpioId == rhs.getGpio())
-  {
-    this->buttonState = rhs.getState();
-    this->timeStamp = rhs.getTimeStamp();
-    success = true;
-  }
-  return success;
+    bool success = false;
+    if (this->gpioId == rhs.getGpio())
+    {
+        this->buttonState = rhs.getState();
+        this->timeStamp = rhs.getTimeStamp();
+        success = true;
+    }
+    return success;
 }
 
-bool Button::timeElapsedExceeds(time_t startTime, uint32_t seconds)
+bool Button::timeElapsedExceeds(datetime_t startTime, uint32_t seconds)
 {
-  return (time(NULL) - startTime) > seconds;
+    datetime_t currentTime;
+    rtc_get_datetime(&currentTime);
+    
+    uint32_t startSeconds = startTime.hour * 3600 + startTime.min * 60 + startTime.sec;
+    uint32_t currentSeconds = currentTime.hour * 3600 + currentTime.min * 60 + currentTime.sec;
+    
+    return (currentSeconds - startSeconds) >= seconds;
 }
--- src/button.h
+++ src/button.h
@@ -11,115 +11,113 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
-#include "common.h"
-#include <cstdint>
-#include <ctime>
-#include <string>
-
+#include "hardware/rtc.h"
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
+#include "common.h"
+#include <cstdint>
+#include <string>
 
 enum ButtonState
 {
-  DEFAULT_STATE,
-  RISING_EDGE,
-  LONG_HOLD,
-  FALLING_EDGE
+    DEFAULT_STATE,
+    RISING_EDGE,
+    LONG_HOLD,
+    FALLING_EDGE
 };
 
 class Button
 {
 public:
-  /**
-   * @brief Creates a button object which will monitor
-   * the specified GPIO.  There is no error checking for
-   * valid GPIO's at the moment.  Partially because I am
-   * lazy and partially because it will very from model
-   * to model.
-   *
-   * @param pio The PIO instance to use
-   * @param gpio GPIO pin number
-   * @param buttonDesc Optional description
-   */
-  Button(PIO pio, uint8_t gpio, std::string buttonDesc = "");
+    /**
+     * @brief Creates a button object which will monitor
+     * the specified GPIO.  There is no error checking for
+     * valid GPIO's at the moment.  Partially because I am
+     * lazy and partially because it will very from model
+     * to model.
+     *
+     * @param pio The PIO instance to use
+     * @param gpio GPIO pin number
+     * @param buttonDesc Optional description
+     */
+    Button(PIO pio, uint8_t gpio, std::string buttonDesc = "");
 
-  /**
-   * @brief Destroy the Button object
-   *
-   */
-  virtual ~Button() = default;
+    /**
+     * @brief Destroy the Button object
+     *
+     */
+    virtual ~Button() = default;
 
-  /**
-   * @brief Update the button state.  Possible values are
-   * -  DEFAULT_STATE
-   * -  RISING_EDGE
-   * -  LONG_HOLD
-   * -  FALLING_EDGE
-   *
-   * @param newValue
-   */
-  void update();
+    /**
+     * @brief Update the button state.  Possible values are
+     * -  DEFAULT_STATE
+     * -  RISING_EDGE
+     * -  LONG_HOLD
+     * -  FALLING_EDGE
+     *
+     * @param newValue
+     */
+    void update();
 
-  /**
-   * @brief Return the buttons GPIO value
-   *
-   * @return uint8_t GPIO
-   */
-  uint8_t getGpio() const;
+    /**
+     * @brief Return the buttons GPIO value
+     *
+     * @return uint8_t GPIO
+     */
+    uint8_t getGpio() const;
 
-  /**
-   * @brief Get the State object
-   *
-   * @return ButtonState
-   */
-  ButtonState getState() const;
+    /**
+     * @brief Get the State object
+     *
+     * @return ButtonState
+     */
+    ButtonState getState() const;
 
-  /**
-   * @brief Get the Time Stamp object
-   *
-   * @return time_t
-   */
-  time_t getTimeStamp() const;
+    /**
+     * @brief Get the Time Stamp object
+     *
+     * @return time_t
+     */
+    datetime_t getTimeStamp() const;
 
-  /**
-   * @brief Get button description
-   *
-   * @return std::string
-   */
-  std::string getButtonDesc() const;
+    /**
+     * @brief Get button description
+     *
+     * @return std::string
+     */
+    std::string getButtonDesc() const;
 
-  /**
-   * @brief If gpio's match, update timestamp and state and
-   * return true.  Otherwise return false
-   *
-   * @param rhs Button
-   * @return true GPIO's match.  Timestamp and stat updated.
-   * @return false No match.  Nothing updated.
-   */
-  bool operator<<(const Button &rhs);
+    /**
+     * @brief If gpio's match, update timestamp and state and
+     * return true.  Otherwise return false
+     *
+     * @param rhs Button
+     * @return true GPIO's match.  Timestamp and stat updated.
+     * @return false No match.  Nothing updated.
+     */
+    bool operator<<(const Button &rhs);
 
 private:
+    bool timeElapsedExceeds(datetime_t startTime, uint32_t seconds);
 
-bool timeElapsedExceeds(time_t startTime, uint32_t seconds);
+    // gpio number which button is acting on
+    uint8_t gpioId;
 
-  // gpio number which button is acting on
-  uint8_t gpioId;
+    // current button state
+    ButtonState buttonState;
 
-  // current button state
-  ButtonState buttonState;
+    // last button update
+    datetime_t timeStamp;
 
-  // last button update
-  time_t timeStamp;
-
-  // button description
-  std::string mButtonDesc;
-  
-  PIO pio;
-  
-  uint sm;
-  
-  uint offset;
-  
+    // button description
+    std::string mButtonDesc;
+    
+    PIO pio;
+    
+    uint sm;
+    
+    uint offset;
+    
 };
 
 #endif /* BUTTON_H */
